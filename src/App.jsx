e1ef2fb7ba4acba4ce98815346391d4df8253766
neginz <negin.z566@gmail.com>
2024-08-06 03:00:20 +0330

    [feat] add App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Tasks from "./components/tasks/Tasks"
+import Tasks02 from "./components/Tasks-2/Tasks02"
 
 const App = () => {
 	return (
-		<div className="container">
-			<Tasks />
+		<div  className="box-container">
+			<Tasks02 />
 		</div>
 	)
 }
