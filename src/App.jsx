59c1d8de6633fac7d6858bc50d9803c83be62026
Salma Erfani <salma.eztt@gmail.com>
2024-08-05 13:16:21 +0330

    [feat] routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import Tasks from "./components/tasks/Tasks"
 
 const App = () => {
 	return (
-		<div className="container">
-			<Tasks />
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Navigate to='/task/list' replace />} />
+				<Route path="task/list" element={<Tasks />} />
+				{/* <Route path="task/create" /> */}
+				{/* <Route path="task/:id/edit" /> */}
+				{/* <Route path="login" /> */}
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
