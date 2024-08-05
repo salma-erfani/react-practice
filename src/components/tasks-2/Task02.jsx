520f2e6283bcc1ef457ccfa1a968dce2be232c6a
neginz <negin.z566@gmail.com>
2024-08-06 03:11:36 +0330

    [feat] add task02 component and style

---
+++ src/components/tasks-2/Task02.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Content from "./Content"
+import Footer from "./Footer"
+import './tasks02.css'
+
+
+const Tasks02 = () => {
+    return (
+        <form className="form-wrap">
+            <Header />
+            <Content />
+            <Footer />
+        </form>
+    )
+}
+
+export default Tasks02
