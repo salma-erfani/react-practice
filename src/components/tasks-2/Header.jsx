179bfec09c9fc55db6c423cc5d5f2be7bd3108cb
neginz <negin.z566@gmail.com>
2024-08-06 03:12:00 +0330

    [feat] add Header component

---
+++ src/components/tasks-2/Header.jsx
@@ -0,0 +1,13 @@
+// import LogoutIcon from '@mui/icons-material/Logout'
+
+const Header = () => {
+  return (
+    <header className="header-form">
+      <img src="./icons/delete.svg" class="icon" />
+      <h2>Edit Task #1</h2>
+      <img src="./icons/arrow_right.svg" class="icon" />
+    </header>
+  );
+};
+
+export default Header;
